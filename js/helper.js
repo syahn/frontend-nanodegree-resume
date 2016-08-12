a8e3de478b9c33d0b7a046e3c8034369a7a3c59a
SY <sy.ahn25@gmail.com>
2016-08-12 12:57:40 +0900

    feat: Finish basic requirements for the project

--- js/helper.js
+++ js/helper.js
@@ -110,9 +110,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -131,7 +131,7 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
       locations.push(school.location);
@@ -139,7 +139,7 @@ function initializeMap() {
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -234,16 +234,13 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,213 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//// 01. Information
+
+var bio = {
+  	"name": "Sangyoung Ahn",
+  	"role": "Front-end Developer",
+  	"contacts": {
+  		"mobile": "+82-10-9508-0900",
+  		"email": "sy.ahn25@gmail.com",
+  		"github": "syahn",
+  		"twitter": "@frankhaus",
+      "blog": "http://syahn.com",
+  		"location": "SouSouth Korea"
+  	},
+  	"welcomeMessage": "Hello~",
+  	"skills": ["HTML/CSS", "JavaScript", "ReactJS", "nodeJS"],
+  	"bioPic": "images/profile.jpg"
+}
+
+var education={
+  	"schools": [{
+  		"name": "Sungkyungwan University",
+  		"location": "Seoul, Suwon",
+  		"degree": "Bachelor",
+  		"dates": "2010-3 ~ ",
+  		"url": "skku.edu",
+  		"majors": ["Oriental philosophy", "Computer Science"]
+  	},{
+      "name": "Sungkwang Highschool",
+  		"location": "Daegu",
+  		"dates": "2007-3 ~ 2010-3",
+  		"url": "skku.edu",
+  		"majors": ["Oriental philosophy", "Computer Science"]
+    }],
+  	"onlineCourses": [{
+  		"title": "Front-end Nanodegree",
+  		"school": "Udacity",
+  		"dates": "2016/8",
+  		"url": "http://udacity.com"
+  	}, {
+  		"title": "Udacity Nanodegree",
+  		"school": "Null",
+  		"dates": "2016/8",
+  		"url": "Udacity.com"
+  	}]
+}
+
+var projects = {
+  	"projects": [{
+  		"title": "member",
+  		"dates": "2016/8/10",
+  		"description": "my first projects",
+  		"images": ["images/fry.jpg"]
+  	}]
+}
+
+var work = {
+  	"jobs": [{
+  		"employer": "청년장사꾼",
+  		"title": "manager",
+  		"location": "seoul",
+  		"dates": "In progress",
+  		"description": "young seller"
+  	}]
+}
+
+//// 02. Formatting objects and appending
+
+
+// 1.bio
+
+bio.display = function(){
+
+  // Role
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  $("#header").prepend(formattedRole);
+
+  // Name
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  $("#header").prepend(formattedName);
+
+  // ContactGeneric - Email
+  var formattedContacGeneric = HTMLcontactGeneric.replace("%contact%", "email");
+  var formattedContacGeneric = formattedContacGeneric.replace("%data%", bio.contacts.email);
+  $("#topContacts").append(formattedContacGeneric);
+
+  // Mobile
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  $("#topContacts").append(formattedMobile);
+
+  // Twitter
+  var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+  $("#topContacts").append(formattedTwitter);
+
+  // Github
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+  $("#topContacts").append(formattedGithub);
+
+  // Blog
+  var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+  $("#topContacts").append(formattedBlog);
+
+  // Location
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  $("#topContacts").append(formattedLocation);
+
+  // Biopic
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+  $("#header").append(formattedBioPic);
+
+  // Welcome message
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+  $("#header").append(formattedWelcomeMsg);
+
+  // Skills
+  if (bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+    bio.skills.forEach(function(skill){
+      var formattedSkills = HTMLskills.replace("%data%", skill);
+      $("#skills").append(formattedSkills);
+    })
+  }
+};
+
+bio.display();
+
+
+// 2. Work
+work.display = function(){
+  work.jobs.forEach(function(job){
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+    var formattedLocation = HTMLworkLocation.replace("%data%", job.location);
+    var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+    var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+
+    $(".work-entry:last").append(formattedEmployer + formattedTitle);
+    $(".work-entry:last").append(formattedDates);
+    $(".work-entry:last").append(formattedLocation);
+    $(".work-entry:last").append(formattedDescription);
+  });
+};
+
+work.display();
+
+
+// 3. Projects
+
+projects.display = function(){
+  projects.projects.forEach(function(project){
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedTitle= HTMLprojectTitle.replace("%data%", project.title);
+    var formattedDates= HTMLprojectDates.replace("%data%", project.dates);
+    var formattedDescription= HTMLprojectDescription.replace("%data%", project.description);
+    var formattedImages= HTMLprojectImage.replace("%data%", project.images);
+
+    $(".project-entry:last").append(formattedTitle);
+    $(".project-entry:last").append(formattedDates);
+    $(".project-entry:last").append(formattedDescription);
+    $(".project-entry:last").append(formattedImages);
+  })
+}
+
+projects.display();
+
+
+// 4. Education
+
+education.display = function(){
+  education.schools.forEach(function(school){
+    $("#education").append(HTMLschoolStart);
+
+    var formattedName= HTMLschoolName.replace("%data%", school.name);
+    var formattedName= formattedName.replace("#", school.url);
+
+    var formattedLocation= HTMLschoolLocation.replace("%data%", school.location);
+    var formattedDegree= HTMLschoolDegree.replace("%data%", school.degree);
+    var formattedDates= HTMLschoolDates.replace("%data%", school.dates);
+
+    $(".education-entry:last").append(formattedName);
+    $(".education-entry:last").append(formattedLocation);
+    $(".education-entry:last").append(formattedDegree);
+    $(".education-entry:last").append(formattedDates);
+
+    school.majors.forEach(function(major){
+      var formattedMajor= HTMLschoolMajor.replace("%data%", major);
+      $(".education-entry:last").append(formattedMajor);
+    })
+  })
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+
+  education.onlineCourses.forEach(function(course){
+    var formattedTitle= HTMLonlineTitle.replace("%data%", course.title);
+    var formattedSchool= HTMLonlineSchool.replace("%data%", course.school);
+    var formattedDates= HTMLonlineDates.replace("%data%", course.dates);
+    var formattedUrl= HTMLonlineURL.replace("%data%", course.url);
+
+    $(".education-entry:last").append(formattedTitle);
+    $(".education-entry:last").append(formattedSchool);
+    $(".education-entry:last").append(formattedDates);
+    $(".education-entry:last").append(formattedUrl);
+  })
+}
+
+education.display();
+
+
+// 5. Map
+
+$("#mapDiv").append(googleMap);
